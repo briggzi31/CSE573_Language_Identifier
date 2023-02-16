0398259e129641e268b711f5259410bb1e2a0fa2
Lexie Wang <travelaroundthemoon@gmail.com>
2023-02-16 12:55:34 -0800

    Update train_test_split.py

--- scripts/train_test_split.py
+++ scripts/train_test_split.py
@@ -1,10 +1,10 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-train_data = '/data/train.csv'
-test_data = '/data/test.csv'
+train_data = 'data/train.csv'
+test_data = 'data/test.csv'
 
-data = pd.read_csv("/data/sentences.csv")
+data = pd.read_csv("data/sentences.csv")
 train, test = train_test_split(data, test_size=0.2, random_state=1)
 
 train.to_csv(train_data)
