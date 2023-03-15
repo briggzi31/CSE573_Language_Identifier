656629ae901358c3d97848ca137e3e60c93ecaaa
Lexie Wang <travelaroundthemoon@gmail.com>
2023-03-14 22:01:07 -0700

    kNN segmentation fault

--- src/classifiers/knn.py
+++ src/classifiers/knn.py
@@ -13,12 +13,15 @@ as a list of predicted labels.
 Usage
 -----
 """
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
 from src.classifiers.classify import Classifier
 from sklearn.neighbors import KNeighborsClassifier
 
 class KNN(Classifier):
     def __init__(self, train_vectors_file, train_gold_labels_file, dev_vectors_file, dev_gold_labels_file,
                  test_vectors_file, test_gold_labels_file):
+        print('Initializing kNN model...')
         super().__init__(train_vectors_file, train_gold_labels_file, dev_vectors_file, dev_gold_labels_file,
                          test_vectors_file, test_gold_labels_file)
 
@@ -26,10 +29,12 @@ class KNN(Classifier):
         return
 
     def train(self):
+        print('Training the kNN model...')
         self.model.fit(self.train_vectors, self.train_gold_labels)
         return
 
     def test(self, use_dev, output_file):
+        print('Testing the kNN model...')
         if use_dev:
             data = self.dev_vectors
             gold_labels = self.dev_gold_labels
@@ -52,16 +57,16 @@ class KNN(Classifier):
 
 # Test
 # train pickle
-train_vectors_file = "pickle_objects/features/train_vectors.pickle"
-train_gold_labels_file = "pickle_objects/gold_labels/train_gold_labels.pickle"
+train_vectors_file = "../../pickle_objects/features/train_vectors.pickle"
+train_gold_labels_file = "../../pickle_objects/gold_labels/train_gold_labels.pickle"
 
 # dev pickle
-dev_vectors_file = "pickle_objects/features/dev_vectors.pickle"
-dev_gold_labels_file = "pickle_objects/gold_labels/dev_gold_labels.pickle"
+dev_vectors_file = "../../pickle_objects/features/dev_vectors.pickle"
+dev_gold_labels_file = "../../pickle_objects/gold_labels/dev_gold_labels.pickle"
 
 # test pickle
-test_vectors_file = "pickle_objects/features/test_vectors.pickle"
-test_gold_labels_file = "pickle_objects/gold_labels/test_gold_labels.pickle"
+test_vectors_file = "../../pickle_objects/features/test_vectors.pickle"
+test_gold_labels_file = "../../pickle_objects/gold_labels/test_gold_labels.pickle"
 
 k = KNN(train_vectors_file, train_gold_labels_file, dev_vectors_file, dev_gold_labels_file, test_vectors_file, test_gold_labels_file)
 k.train()
